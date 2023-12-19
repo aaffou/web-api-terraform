35ac037066bc25c30cedeb7e7db9af570465c572
AdamAffou <adam.affou@gmail.com>
2023-12-19 23:55:23 +0100

    working aws environment

--- server/services/downloadsDynamo.ts
+++ server/services/downloadsDynamo.ts
@@ -1,11 +1,9 @@
-import * as AWS from 'aws-sdk'
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import { DynamoDBDocumentClient, UpdateCommand, GetCommand } from '@aws-sdk/lib-dynamodb'
 import { EnumfileToUse } from '~/types/types'
 
-AWS.config.update({ region: 'us-west-2' })
-
-// eslint-disable-next-line import/namespace
-const ddb = new AWS.DynamoDB.DocumentClient()
-
+const ddbClient = new DynamoDBClient({ region: 'eu-west-1' }) // Change to your region
+const docClient = DynamoDBDocumentClient.from(ddbClient)
 const toDoTableName = 'to-do-downloads'
 const doneTableName = 'done-downloads'
 const recordId = 'urls_record'
@@ -13,16 +11,15 @@ const recordId = 'urls_record'
 export async function addUrlToDynamo (url: string, tableToUse: keyof typeof EnumfileToUse): Promise<void> {
   const params = {
     TableName: getFilePath(tableToUse),
-    Key: { id: recordId },
+    Key: { urls_record: recordId },
     UpdateExpression: 'ADD urls :url',
     ExpressionAttributeValues: {
-      ':url': ddb.createSet([url])
-    },
-    ReturnValues: 'UPDATED_NEW'
+      ':url': new Set([url])
+    }
   }
 
   try {
-    await ddb.update(params).promise()
+    await docClient.send(new UpdateCommand(params))
     console.log(`URL added: ${url}`)
   } catch (error) {
     console.error(`Error adding URL: ${error}`)
@@ -32,12 +29,12 @@ export async function addUrlToDynamo (url: string, tableToUse: keyof typeof Enum
 export async function listUrlsToDynamo (tableToUse: keyof typeof EnumfileToUse): Promise<string[]> {
   const params = {
     TableName: getFilePath(tableToUse),
-    Key: { id: recordId }
+    Key: { urls_record: recordId }
   }
 
   try {
-    const data = await ddb.get(params).promise()
-    return data.Item ? data.Item.urls.values : []
+    const data = await docClient.send(new GetCommand(params))
+    return data.Item ? Array.from(data.Item.urls) : []
   } catch (error) {
     console.error(`Error listing URLs: ${error}`)
     return []
@@ -47,15 +44,15 @@ export async function listUrlsToDynamo (tableToUse: keyof typeof EnumfileToUse):
 export async function deleteUrlToDynamo (url: string, tableToUse: keyof typeof EnumfileToUse): Promise<void> {
   const params = {
     TableName: getFilePath(tableToUse),
-    Key: { id: recordId },
+    Key: { urls_record: recordId },
     UpdateExpression: 'DELETE urls :url',
     ExpressionAttributeValues: {
-      ':url': ddb.createSet([url])
+      ':url': new Set([url])
     }
   }
 
   try {
-    await ddb.update(params).promise()
+    await docClient.send(new UpdateCommand(params))
     console.log(`URL deleted: ${url}`)
   } catch (error) {
     console.error(`Error deleting URL: ${error}`)
